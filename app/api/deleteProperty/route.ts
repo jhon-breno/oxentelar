5d28c52aaff6e8a390871a433478819a202a8d9a
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-11-30 23:38:39 -0300

    fix: deletePropertyWithValidation not used

--- app/api/deleteProperty/route.ts
+++ app/api/deleteProperty/route.ts
@@ -1,4 +1,6 @@
+// app/api/deletePropertyWithValidation/route.ts
 import { prisma } from "@/prisma/prisma"
+import { NextResponse } from "next/server"
 
 async function deletePropertyWithValidation(propertyId: string) {
   try {
@@ -26,5 +28,32 @@ async function deletePropertyWithValidation(propertyId: string) {
   } catch (error) {
     console.error("Erro ao excluir imóvel:", error)
     throw error
+  }
+}
+
+export async function DELETE(request: Request) {
+  try {
+    const { propertyId } = await request.json()
+
+    if (!propertyId) {
+      return NextResponse.json(
+        { error: "O ID do imóvel é obrigatório." },
+        { status: 400 },
+      )
+    }
+
+    // Chama a função de validação e exclusão
+    await deletePropertyWithValidation(propertyId)
+
+    return NextResponse.json(
+      { message: "Imóvel excluído com sucesso!" },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.error("Erro ao processar a requisição:", error)
+    return NextResponse.json(
+      { error: "Erro ao excluir imóvel. Por favor, tente novamente." },
+      { status: 500 },
+    )
   }
 }
