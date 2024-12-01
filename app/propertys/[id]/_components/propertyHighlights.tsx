cc9425c8fe0ff5cdc67234a5389d4c1c797426a2
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-12-01 00:12:12 -0300

    fix: remove variable not used

--- app/propertys/[id]/_components/propertyHighlights.tsx
+++ app/propertys/[id]/_components/propertyHighlights.tsx
@@ -9,7 +9,7 @@ const PropertyHighligths = ({ highligths }: PropertyHighligthProps) => {
     <div className="flex flex-col p-5">
       <h2 className="mb-2 font-semibold text-primaryDarker">Destaques</h2>
       <div className="flex flex-wrap gap-y-3">
-        {highligths.map((highligth, index) => (
+        {highligths.map((highligth) => (
           <div key={highligth} className="flex w-1/2 items-center gap-2">
             <CircleCheck className="text-primary" width={16} height={16} />
 
